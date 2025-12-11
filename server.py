992046be6d372d1ab39ec6ea6c3c8b20090728df
JoeL-ThoMaS-Chacko <joelthomaschacko240@gmail.com>
2025-12-11 08:14:26 +0530

    Implement CORS middleware for FastAPI
    Added CORS middleware to allow cross-origin requests.

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
 import joblib
 import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
 
 model = joblib.load('app/model.joblib')
 le = joblib.load('app/label_encoder.joblib')
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins 
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 @app.get('/')
 def home():
     return {"message": "Crop Prediction API - Send GET request to /predict"}
@@ -14,11 +21,10 @@ def home():
 def predict(ph: float, n: float, p: float, k: float, 
             temp: float, humidity: float, rainfall: float):
     
-    # Create feature array
     features = np.array([[ph, n, p, k, temp, humidity, rainfall]])
      
-    # Make prediction
     prediction = model.predict(features)
     crop = le.inverse_transform(prediction)
     
+
     return {"predicted_crop": crop[0].item()}
